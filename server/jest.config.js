966fae46c2d8b3c37954158dc30fe4e554dd4818
JayC <caballerojavier13@gmail.com>
2023-08-09 17:40:33 -0300

    CI: Coverage report

--- server/jest.config.js
+++ server/jest.config.js
@@ -13,6 +13,7 @@ module.exports = {
   testMatch: [
     '**/test/**/*.unit.test.js'
   ],
+  reporters: ["default", "jest-junit"],
   collectCoverageFrom: [
     '**/src/controllers/*.js',
     // '**/src/middleware/*.js',
