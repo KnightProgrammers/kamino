c192d78761b0982191142f66cbcff5598f3889c9
JayC <caballerojavier13@gmail.com>
2023-08-11 12:47:32 -0300

    Fix LOG_LEVEL environment config

---
+++ server/src/config/log.config.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+module.exports = {
+  LOG_LEVEL: process.env.LOG_LEVEL || 'INFO',
+};
--- server/src/utils/logger.js
+++ server/src/utils/logger.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 
 const { createLogger, transports, format } = require('winston');
+const config = require('../config/log.config');
 
 const logger = createLogger({
-  level: process.env.LOG_LEVEL.toLowerCase() || 'info',
+  level: config.LOG_LEVEL.toLowerCase(),
   format: format.combine(
     format.printf(info => `[${info.level.toUpperCase()}]: ${info.message}`),
     format.colorize({all: true})
