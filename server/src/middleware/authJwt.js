031fb336efec400b66917bf0592badddc9797e86
JayC <caballerojavier13@gmail.com>
2023-08-14 20:22:50 -0300

    Warnings are not errors

--- server/src/middleware/authJwt.js
+++ server/src/middleware/authJwt.js
@@ -14,15 +14,15 @@ const verifyUser = async (userId) => {
 
 const catchError = (err, res, next) => {
   if (err instanceof TokenExpiredError) {
-    next(errorBuilder('Unauthorized - Access Token was expired', 401));
+    next(errorBuilder('Unauthorized - Access Token was expired', 401, 'warn'));
   }
-  next(errorBuilder('Unauthorized', 401));
+  next(errorBuilder('Unauthorized', 401, 'warn'));
 }
 
 const verifyToken = async (req, res, next) => {
   let token = req.headers["x-access-token"];
   if (!token) {
-    return next(errorBuilder('No token provided', 403));
+    return next(errorBuilder('No token provided', 403, 'warn'));
   }
 
   const userId = await RedisClient.get(token);
@@ -38,10 +38,10 @@ const verifyToken = async (req, res, next) => {
     config.secret,
     async (err, decoded) => {
       if (err) {
-        return next(errorBuilder('Unauthorized', 401));
+        return next(errorBuilder('Unauthorized', 401, 'warn'));
       }
       const userExist = await verifyUser(decoded.id);
-      if (!userExist) return next(errorBuilder('Unauthorized', 401));
+      if (!userExist) return next(errorBuilder('Unauthorized', 401, 'warn'));
       req.userId = decoded.id;
       newrelic.setUserID(decoded.id);
       next();
