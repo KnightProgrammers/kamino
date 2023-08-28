0876736e7588210b28b7494ec28a8fdbb342454e
Javier Caballero <caballerojavier13@gmail.com>
2023-08-28 20:15:13 -0300

    New Refresh token logic (#30)

--- server/src/validators/auth.validators.js
+++ server/src/validators/auth.validators.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 const validator = require('express-joi-validation').createValidator({passError: true});
 
 const BODY_CONF = {
-  joi: { convert: true, allowUnknown: false, abortEarly: true }
+  joi: {convert: true, allowUnknown: false, abortEarly: true}
 };
 
 const signupSchema = Joi.object({
@@ -17,11 +17,11 @@ const loginSchema = Joi.object({
 }).required();
 
 const refreshTokenSchema = Joi.object({
-  refreshToken: Joi.string().uuid({version: 'uuidv4'}).trim(true).required().label("Refresh Token"),
+  refreshToken: Joi.string().trim(true).required().label("Refresh Token"),
 }).required();
 
 module.exports = {
-  signupValidator: validator.body(signupSchema,BODY_CONF),
-  loginValidator: validator.body(loginSchema,BODY_CONF),
-  refreshTokenValidator: validator.body(refreshTokenSchema,BODY_CONF)
+  signupValidator: validator.body(signupSchema, BODY_CONF),
+  loginValidator: validator.body(loginSchema, BODY_CONF),
+  refreshTokenValidator: validator.body(refreshTokenSchema, BODY_CONF)
 }
